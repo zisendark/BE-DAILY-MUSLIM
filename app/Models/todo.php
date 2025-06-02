4b0b984b9f78f679e5bcbc2b76366ddccd41206e
zisendark <muhammad.althaaf127@smk.belajar.id>
2025-06-02 09:35:26 +0700

    update lagi

---
+++ app/Models/todo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class todo extends Model
+{
+    //
+    protected $table = 'todos';
+
+    protected $fillable = ['nama', 'kategori', 'tanggal'];
+}
